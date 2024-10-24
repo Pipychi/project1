35df3a71c75e3cd3d2f5c3a709481431614d6a8c
Polina <145287069+Pipychi@users.noreply.github.com>
2024-10-24 23:49:24 +0500

    Add files via upload

---
+++ Лабораторная работа 2.23/Лабораторная работа 2.23/Point.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лабораторная_работа_2._23
+{
+    internal class Point
+    {
+        //поля(координаты точки)
+        private double x;
+        private double y;
+
+        //свойства
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        //конструктор
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        //метод, который вычисляет расстояние от точки до начала координат ( по формуле √(x² + y²))
+        public double Distance()
+        {
+            return Math.Sqrt(X * X + Y * Y);
+        }
+
+        //перегрузка метода ToString для вывода полей
+        public override string ToString()
+        {
+            return $"Координаты точки: ( {X} ; {Y})";
+        }
+
+        //ПЕРЕГРУЗКИ ОПЕРАТОРОВ
+        //1. Унарная операция: уменьшает координаты x и y на 1
+        public static Point operator --(Point p)   /* -- префиксный оператор декремента*/
+        {
+            p.X = p.X - 1;
+            p.Y = p.Y - 1;
+            return p;
+        }
+        //унарная операция: поменять местами координаты x и y
+        public static Point operator ~ (Point p)
+        {
+            double z = p.X;
+            p.X = p.Y;
+            p.Y = z;
+            return p;
+        }
+
+        //2. Операции приведения типа
+        //int(неявная) - результатом является целая часть координаты x
+        public static implicit operator int (Point p)
+        {
+            return (int) p.X; /*возвращаем целую часть x*/
+        }
+        //double(явная) - результатом является координата y
+        public static explicit operator double (Point p)
+            { return  p.Y; }
+
+        //3. Бинарные операции
+        //левосторонняя операция, уменьшается координата х
+        public static Point operator - (Point p, int value)
+        {
+            p.X -= value;
+            return p ;
+        }
+        //правосторонняя операция, уменьшается координата y
+        public static Point operator -(int value, Point p)
+        {
+            p.Y -= value;
+            return p ;  
+        }
+        //вычисляется расстояние до точки p ( по формуле Пифагора: r =√((a-x)² + (b - y)²), где (x;y) p = (a;b))
+        public static double operator - (Point p1, Point p2)
+        {
+            return Math.Sqrt(Math.Pow(p2.X - p1.X, 2) + Math.Pow(p2.Y - p1.Y, 2));
+        }
+    }
+}
---
+++ Лабораторная работа 2.23/Лабораторная работа 2.23/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лабораторная_работа_2._23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+          
+            // Ввод данных с проверкой корректность ввода 
+            Console.WriteLine("Вам необходимо ввести координаты точки (они могут принимать дробное значение)");
+
+            while (true)
+            {
+                Console.Write("Введите X: ");
+                if (double.TryParse(Console.ReadLine(), out x)) /*проверяем, можно ли получить число типа  double из строки */
+                    break;
+                else
+                {
+                    Console.WriteLine("Ошибка ввода. Заново введите числовое значение X.");
+                }
+                
+            }
+
+            while (true)
+            {
+                Console.Write("Введите Y: ");
+                if (double.TryParse(Console.ReadLine(), out y)) /*проверяем, можно ли получить число типа  double из строки */
+                    break;
+                else
+                {
+                    Console.WriteLine("Ошибка ввода. Заново введите числовое значение Y.");
+                }
+            }
+
+            //создание объекта Point
+            Point point = new Point(x, y);
+            // Вывод информации о точке
+            Console.WriteLine(point.ToString());
+
+            // Вычисление расстояния до начала координат
+            double distance = point.Distance();
+            //вывод результата на экран 
+            Console.WriteLine("Расстояние от точки до начала координат:" + distance);
+
+            //ТЕСТИРОВАНИЕ УНАРНЫХ ОПЕРАЦИЙ 
+            Console.WriteLine("\nПерегруженные операции.\n 'Унарные операции'");
+            //уменьшение координат на 1
+            point--;
+            Console.WriteLine("Уменьшение координат на 1: " + point.ToString());
+
+            //перестановка координат между друг другом
+            point = ~point;
+            Console.WriteLine("Перестановка координат: " + point.ToString());
+
+            //ТЕСТИРОВАНИЕ ОПЕРАЦИЙ ПРИВЕДЕНИЯ ТИПА
+            Console.WriteLine("\n 'Операции приведения типа'");
+            int xInt = point;  /*неявное приведение*/
+            Console.WriteLine("Целая часть x = " + xInt);
+
+            double yDouble = (double)point; /*явное приведение*/
+            Console.WriteLine("Координата y = " + yDouble);
+
+            //ТЕСТИРОВАНИЕ БИНАРНЫХ ОПЕРАЦИЙ
+            Console.WriteLine("\n 'Бинарные операции'");
+            int a;
+            Console.Write("Введите число, на которое хотите уменьшить x: ");
+            a = int.Parse(Console.ReadLine());
+            point = point - a;
+            Console.WriteLine("" + point);
+
+            int b;
+            Console.Write("Введите число, на которое хотите уменьшить y: ");
+            b = int.Parse(Console.ReadLine());
+            point = b - point;
+            Console.WriteLine("" + point);
+
+            Console.WriteLine("Вычисление расстояния от координаты до точки p = (3;3) ");
+            //берем в пример координату p = (3;3)
+            Point p = new Point(3, 3);
+            double distancP = point - p;
+            Console.WriteLine("Расстояние между точками = " + distancP);
+
+
+        }
+    }
+}
